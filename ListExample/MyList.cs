b483c1f4c8398a9ca4ec482d2cc4b9db4128aaf6
serkan özel <serkanozel399@gmail.com>
2021-05-12 02:21:50 +0300

    Add project files.

---
+++ ListExample/MyList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExample
+{
+    class MyList<K, V>
+    {
+        K[] keys;
+        K[] keysArray;
+        V[] values;
+        V[] valuesArray;
+
+
+
+        public MyList()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            keysArray = keys;
+            keys = new K[keys.Length + 1];
+            for (int i = 0; i < keysArray.Length; i++)
+            {
+                keys[i] = keysArray[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            valuesArray = values;
+            values = new V[values.Length + 1];
+            for (int i = 0; i < valuesArray.Length; i++)
+            {
+                values[i] = valuesArray[i];
+            }
+            values[values.Length - 1] = value;
+        }
+
+        public int KLength
+        {
+            get { return keys.Length; }
+        }
+        public K[] key
+        { 
+            get { return keys; }
+        }
+
+        public int VLength
+        {
+            get { return values.Length; }
+        }
+        public K[] value
+        {
+            get { return value; }
+        }
+    }
+}
---
+++ ListExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            MyList<string,string> liste = new MyList<string,string>();
+            liste.Add("Araba","Yeşil");
+            liste.Add("Ev", "Turuncu");
+
+            Console.WriteLine(liste.KLength);
+            Console.WriteLine(liste.VLength);
+
+            foreach (var item in liste.key)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in liste.value)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
